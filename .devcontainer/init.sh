ae974f6acb80fc99a02ce953a4d6a1a43ca35ed3
diogobaltazar <d.ogobaltazar+github@gmail.com>
2026-02-01 15:37:46 +0000

    feat(devcontainer): add util init script

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 # first time project setup
+docker compose -f .devcontainer/compose.yml build base
+docker compose -f .devcontainer/compose.yml build tools
+USRID=$(id -u) USRNAME=$(whoami) docker compose -f .devcontainer/compose.yml build --no-cache --pull=false
